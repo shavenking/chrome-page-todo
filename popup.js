89f2a4dbe3041d3767c6ae1323587cf985299b7c
Hugh <shavenking@gmail.com>
2017-04-25 09:42:53 +0800

    Getting started

---
+++ popup.js
@@ -0,0 +1,14 @@
+// document.getElementById('new-task-input').addEventListener('input', function (e) {
+//     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+//         chrome.storage.sync.get(function (items) {
+//             var newItems = items || {};
+
+//             newItems[tabs[0].url] = (newItems[tabs[0].url] || []).push(e.target.value)
+
+//             chrome.storage.sync.set(newItems, function (items) {
+//                 console.log(items)
+//             })
+//         });
+//     })
+// })
+alert('hello');
